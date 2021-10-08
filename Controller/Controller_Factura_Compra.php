c66351d392486aded10031c05018010a39e8c19f
nexcors <guillermocm1998@gmail.com>
2021-10-08 01:03:36 -0300

    muestra de datos

--- Controller/Controller_Factura_Compra.php
+++ Controller/Controller_Factura_Compra.php
@@ -105,10 +105,26 @@ class Controller_Factura_Compra
     }
     public function Read_single_Factura_Compra_para_detalles()
     {
+
         $query = "SELECT * FROM detalle_compra INNER join producto on detalle_compra.producto_id_producto=producto.id_producto where detalle_compra.id_compra=?";
         $stmt = $this->conn->prepare($query);
         //Bind id
         $stmt->bindParam(1, $this->id_compra);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        
+
+        //resto de la query
+        $this->id_detalle_compra = $row['id_detalle_compra'];
+        $this->descripcion_compra_producto = $row['descripcion_compra_producto'];
+        $this->cantidad_compra_producto = $row['cantidad_compra_producto'];
+        $this->valor = $row['valor'];
+        $this->producto_id_producto = $row['producto_id_producto'];
+        $this->id_producto = $row['id_producto'];
+        $this->nombre_producto = $row['nombre_producto'];
+        $this->valor_producto = $row['valor_producto'];
+
 
         try {
             if ($stmt->execute()) {
